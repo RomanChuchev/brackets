6fd43db692ad502b5537bab8adc56e5291adeb6b
romanchuchev <romanok20@gmail.com>
2023-01-17 20:28:20 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, brackets) {
+  const stack = [];
+  const openBr = brackets.map((el) => el[0]);
+  const pathBr = {};
+  for (let el of brackets) {
+    pathBr[el[1]] = el[0];
+  }
+  for (let i = 0; i < str.split("").length; i++) {
+    const current = str.split("")[i];
+
+    if (openBr.includes(current) && pathBr[current] !== current) {
+      stack.push(current);
+    } else if (pathBr[current] === current) {
+      let top = stack[stack.length - 1];
+
+      if (pathBr[current] === top) {
+        stack.pop();
+      } else stack.push(current);
+    } else if (current in pathBr) {
+      if (stack.length === 0) return false;
+
+      let top = stack[stack.length - 1];
+
+      if (pathBr[current] === top) {
+        stack.pop();
+      } else return false;
+    }
+    console.log(current, stack);
+  }
+  return stack.length === 0;
+};
